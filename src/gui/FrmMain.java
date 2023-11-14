6cc9f2f45be61cfdad927a3a1befc28eb55bd9ba
elenavaz <elena.vazrodri@alum.uca.es>
2023-11-14 18:43:12 +0100

    Practica4 falta revisar

---
+++ src/gui/FrmMain.java
@@ -0,0 +1,84 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FrmMain {
+
+	private JFrame _frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmMain window = new FrmMain();
+					window._frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
+	 */
+	public FrmMain() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(
+				UIManager.getSystemLookAndFeelClassName());
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		_frame = new JFrame();
+		_frame.setTitle("Viajes Ciudad Reloj");
+		_frame.setBounds(100, 100, 450, 300);
+		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		_frame.setJMenuBar(menuBar);
+		
+		JMenu mnuNuevo = new JMenu("Nuevo");
+		menuBar.add(mnuNuevo);
+		
+		JMenuItem mitNuevaExcursion = new JMenuItem("Excursión");
+		mitNuevaExcursion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { ShowInternalFrame(new IfrExcursion(), 10, 27, 400, 192); }
+		});
+		mnuNuevo.add(mitNuevaExcursion);
+		
+		JMenu mnuBuscar = new JMenu("Buscar");
+		menuBar.add(mnuBuscar);
+		
+		JMenuItem mitBuscarExcursion = new JMenuItem("Excursión");
+		mnuBuscar.add(mitBuscarExcursion);
+		_frame.getContentPane().setLayout(null);
+	}
+	
+	public void ShowInternalFrame(
+		JInternalFrame ifr, int iX, int iY, int iWidth, int iHeight) {
+		ifr.setBounds(iX, iY, iWidth, iHeight);
+		_frame.getContentPane().add(ifr);
+		ifr.setVisible(true);
+	}
+
+}
---
+++ src/gui/IfrExcursion.java
@@ -0,0 +1,69 @@
+package gui;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+
+import data.Excursion;
+
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class IfrExcursion extends JInternalFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField txtNombre;
+	private Excursion _excursion = null;
+	
+	/**
+	 * Create the frame.
+	 */
+	public IfrExcursion() {
+		setResizable(true);
+		setClosable(true);
+		setTitle("Excursión");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblMensaje = new JLabel("");
+		lblMensaje.setForeground(new Color(0, 0, 255));
+		lblMensaje.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMensaje.setBounds(10, 0, 418, 13);
+		getContentPane().add(lblMensaje);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNombre.setBounds(39, 40, 50, 13);
+		getContentPane().add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setHorizontalAlignment(SwingConstants.LEFT);
+		txtNombre.setBounds(119, 37, 200, 19);
+		getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JButton butGuardar = new JButton("Guardar");
+		butGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if(_excursion == null)
+						_excursion = new Excursion(txtNombre.getText());
+					else
+						_excursion.setName(txtNombre.getText());
+					_excursion.Save();
+				}
+				catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "Ha ocurrido un error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+		});
+		butGuardar.setBounds(39, 77, 85, 21);
+		getContentPane().add(butGuardar);
+
+	}
+}
